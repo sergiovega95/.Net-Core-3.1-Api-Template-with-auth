74352367fcaea79934f02d5ee0f1fe348a92aeef
Sergio Vega <sergiovega9511@gmail.com>
2020-08-07 16:08:20 -0500

    Agregar archivos de proyecto.

---
+++ Infraestructure/IdentityContext/IdentityContext.cs
@@ -0,0 +1,20 @@
+﻿using Core.Entities.Identity;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Security.Cryptography.X509Certificates;
+using System.Text;
+
+namespace Infrastructure.IdentityContext
+{
+    public class IdentityContext:DbContext
+    {
+        public IdentityContext(DbContextOptions<IdentityContext> options)
+            : base(options)
+        {
+            
+        }
+
+        public DbSet<User> UserIdentity { get; set; }
+    }
+}
---
+++ WebApi/Startup.cs
@@ -0,0 +1,78 @@
+using Core.Entities.Identity;
+using Infrastructure.IdentityContext;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using Microsoft.OpenApi.Models;
+using System;
+
+namespace WebApi
+{
+    public class Startup
+    {
+        public Startup(IConfiguration configuration)
+        {
+            Configuration = configuration;
+        }
+
+        public IConfiguration Configuration { get; }
+
+        // This method gets called by the runtime. Use this method to add services to the container.
+        public void ConfigureServices(IServiceCollection services)
+        {          
+            //services.AddDbContext<IdentityContext>(options =>
+            //   options.UseSqlServer(
+            //       Configuration.GetConnectionString("DatabaseConnection")));
+
+            services.AddControllers();
+
+            services.AddSwaggerGen(options =>
+            {
+                var groupName = "v1";
+
+                options.SwaggerDoc(groupName, new OpenApiInfo
+                {
+                    Title = $"Api {groupName}",
+                    Version = groupName,
+                    Description = "Api",
+                    Contact = new OpenApiContact
+                    {
+                        Name = "Foo Company",
+                        Email = string.Empty,
+                        Url = new Uri("https://foo.com/"),
+                    }
+                });
+            
+            });
+        }
+
+        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+        public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
+        {
+            if (env.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+            }
+
+            app.UseHttpsRedirection();
+
+            app.UseRouting();
+
+            app.UseAuthorization();
+
+            app.UseEndpoints(endpoints =>
+            {
+                endpoints.MapControllers();
+            });
+
+            app.UseSwagger();
+            app.UseSwaggerUI(c =>
+            {
+                c.SwaggerEndpoint("/swagger/v1/swagger.json", "Web API V1");
+            });
+        }
+    }
+}

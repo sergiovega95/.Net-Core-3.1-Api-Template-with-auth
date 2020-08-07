40cbc76d51c9fffa2d1e553f789579b60cb2086a
Sergio Vega <sergiovega9511@gmail.com>
2020-08-07 18:58:37 -0500

    identity

---
+++ WebApi/Controllers/IdentityController.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Threading.Tasks;
+using Core.Entities.Identity;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+
+// For more information on enabling Web API for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace WebApi.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class IdentityController : ControllerBase
+    {
+        private readonly SignInManager<User> _signInManager;
+        private readonly UserManager<User> _userManager;
+
+        public IdentityController(UserManager<User> userManager, SignInManager<User> signInManager)
+        {
+            _userManager = userManager;
+            _signInManager = signInManager;
+        }
+
+        [HttpPost]
+        public async Task<ObjectResult> RegisterUser (string email, string password)
+        {
+            var user = new User { UserName = email, Email = password };
+            var result = await _userManager.CreateAsync(user, password);
+            
+            if (result.Succeeded)
+            {
+                
+
+            }
+            else
+            {
+
+            }
+
+        }
+
+
+        [HttpPost]
+        public void SignIn(User user)
+        {
+        }
+
+        
+    }
+}
